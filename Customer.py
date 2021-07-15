0b824d6d5f0d0f0c999892b5a3753a6a88611797
vsaeide <vsaeide7798@gmail.com>
2021-07-15 05:30:54 +0430

    edit ca, customer and ,merchant

--- Customer.py
+++ Customer.py
@@ -1,33 +1,41 @@
-
-
 from Utils import *
 from CA import CA
 
+
 class Customer:
-    def __init__(self,nationalId,certificate_num):
-        self.national_id = nationalId
-        self.certificate_num=certificate_num
-        self.wallet_balance = 0
+    def __init__(self, Id, certificate_num):
+        self.id = Id # account number
+        self.certificate_num = certificate_num
+        #self.wallet_balance = 0
+        ##############
 
-
-
-
-
+        self.public_key = None
+        self.private_key = None
 
     def pub_prv_key_request_to_ca(self, ca_pub_key):
-        message = str(self.national_id) + ", " + str(self.certificate_num)
-        return encrypt(message=message, key=ca_pub_key)
+        message = str(self.id) + ", " + str(self.certificate_num)
+
+        packet = encrypt(message=message, key=ca_pub_key)
+        # send packet to ca and asign keys
+        return packet
+
+    def policy(self, range, count, time, receiver):
+        return range + ','+count+','+time+','+receiver
 
 
-    def send_delegation_to_BC(self,customer_pu_key,bank_pu_key , policy,key_pairs):
+    # create message
+    def send_delegation_to_BC(self, customer_pu_key, bank_pu_key, policy):
 
-            # policy = range + ','+count+','+time+','+receiver
-            self.key_pairs = key_pairs
+        m = (customer_pu_key) + ',' + policy
 
-            m = (customer_pu_key)+','+policy
-            signed = sign(m ,self.key_pairs)
-            message =customer_pu_key +',' +bank_pu_key+','+policy+signed
-            return message
+        signed = sign(m, self.private_key)
+        message = customer_pu_key + ',' + bank_pu_key + ',' + policy + signed
 
+        return message
 
 
+# test
+# ca = CA()
+# my_customer = Customer(5, 223)
+#
+# print(my_customer.pub_prv_key_request_to_ca(ca.get_pub_key()))
--- Main.py
+++ Main.py
@@ -22,25 +22,28 @@ def  payment_protocle(ca,customer,bank,merchant,blockchain,exch_center):
     """
     CA responses to request, by checking credentials & generating prv pub key if it is a new registration
     """
-    ca.response_to_pub_prv_key_request(message_from_customer_to_ca)
+    customer_keys= ca.response_to_pub_prv_key_request(message_from_customer_to_ca)
 
-
+    #????
+    customer.public_key=customer_keys.public_key()
+    customer.private_key=customer_keys
 
     """
     customer send a delidation message for blockchain
     gets pkd , pkm 
     """
-    policy = "100/3/120/111"
-    delidation_message_to_BC = customer.send_delegation_to_BC(ca.get_pub_key_of(customer.national_id),
+
+    policy = customer.policy(100,200,12,'01245')
+    delidation_message_to_BC = customer.send_delegation_to_BC(ca.get_pub_key_of(customer.id),
                                                               ca.get_pub_key_of("Bank"),
                                                               policy,
-                                                              ca.get_key_pairs(customer.national_id))
+                                                              ca.get_key_pairs(customer.id))
 
     """
     BlockChain sends verification message
     """
 
-    blockchain.BC_response_to_deligation(delidation_message_to_BC,ca.get_pub_key_of(customer.national_id))
+    blockchain.BC_response_to_deligation(delidation_message_to_BC,ca.get_pub_key_of(customer.id))
 
     """
     merchant sends request to CA to get pub key & prv key - pkm,skm
@@ -53,8 +56,9 @@ def  payment_protocle(ca,customer,bank,merchant,blockchain,exch_center):
     CA responses to request, by checking credentials & generating prv pub key if it is a new registration
     """
 
-    ca.response_to_pub_prv_key_request(message_from_merchant_to_ca)
-
+    merchant_keys= ca.response_to_pub_prv_key_request(message_from_merchant_to_ca)
+    merchant.public_key=merchant_keys.public_key()
+    merchant.private_key=merchant_keys
 
 
 
@@ -66,11 +70,11 @@ if __name__ == '__main__':
     customer = Customer("12","a20")
     bank = ca.create_Bank()
     merchant = Merchant("01245","679")
-    bank.create_acount(customer.national_id)
-    bank.create_acount(merchant.nationalID)
+    bank.create_acount(customer.id)
+    bank.create_acount(merchant.id)
 
     blockchain = BlockChain()
-    exch_center = ExchangeCenter()
+    exch_center = ExchangeCenter(12)
 
     payment_protocle(ca,customer,bank,merchant,blockchain,exch_center)
 
